026abfe7b5b0024e0f2db1a2a064c32c23226d87
红烧肉热馒头 <1356772315@qq.com>
2020-06-15 16:16:15 +0800

    完成基本布局，完成登录功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store/index'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$store = store
+import { postRequest } from "./utils/api";
+import { postKeyValueRequest } from "./utils/api";
+import { putRequest } from "./utils/api";
+import { deleteRequest } from "./utils/api";
+import { getRequest } from "./utils/api";
+
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.postKeyValueRequest = postKeyValueRequest;
+Vue.prototype.putRequest = putRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.getRequest = getRequest;
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
